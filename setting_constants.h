87d1cf0e7942715bf5d640ee9ff2c04f7dac849d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-24 06:16:02 +0000

    code added

---
+++ setting_constants.h
@@ -0,0 +1,23 @@
+#ifndef CONSTANT_H
+
+#define CONSTANT_H
+
+const int TIME_DELAY_BETWEEN_FRAMES = 100000;   // microseconds, time taken by one frame to execute
+const int UP = 'w', 
+          DOWN = 's', 
+          LEFT = a, 
+          RIGHT = 'd';
+
+// const int ARROW_UP = 'w', 
+//           ARROW_DOWN = 's', 
+//           ARROW_LEFT = a, 
+//           ARROW_RIGHT = 'd';
+
+const int BEFORE_START = 0, 
+          STARTED = 1,
+          Ended = 2;
+
+const FOOD_COUNT = 5;
+
+#endif
+
